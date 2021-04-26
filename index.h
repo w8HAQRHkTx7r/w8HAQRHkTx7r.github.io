f3ad2fdc8f972e22ba6bc7bf12054c168ba66325
Michael Macewich <maceww6438@gmail.com>
2021-04-26 14:47:57 -0500

    markdown

---
+++ index.h
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+      <meta charset="UTF-8">
+      <title>Internal Ad hoc Builds</title>
+    </head>
+    <body>
+      <h1>Where is the sun</h1>
+      <h2>Version 1.06 (Ad hoc)</h2>
+        <li>
+          Publish Date: Sep 12, 2019
+        </li>
+        <li>
+          <a href="itms-services://?action=download-manifest&url=https://USERNAME.github.io/raywenderlich/manifest.plist">Install Build</a>
+        </li>
+        <li>
+          <a href="https://github.com/razeware/emitron-iOS/releases/tag/v1.0.6">Release Notes</a>
+        </li>
+      <h1>When is twilight</h1>
+      <h2>Version 1.06 (Ad hoc)</h2>
+        <li>
+          Publish Date: Dec 27, 2020
+        </li>
+        <li>
+          <a href="itms-services://?action=download-manifest&url=https://USERNAME.github.io/raywenderlich/manifest.plist">Install Build</a>
+        </li>
+        <li>
+          <a href="https://github.com/razeware/emitron-iOS/releases/tag/v1.0.6">Release Notes</a>
+        </li>
+    </body>
+</html>
--- index.md
+++
@@ -1,7 +0,0 @@
-## welcome
-welcome, welcome
-### Markdown
-# Header 1
-## Header 2
-### Header 3
-
